3cf2da769f2f270745b1a0b10f6a8a1d3842590c
Muhammad Ahsan Khan <56512649+mahsankhaan@users.noreply.github.com>
2023-09-05 19:37:44 +0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
  
 mkdir -p /dev/net
+
+chmod 777 /dev/net/tun
+
 mknod /dev/net/tun c 10 200
-chmod 600 /dev/net/tun
+chmod 777 /dev/net/tun
 cd /etc/openvpn/
 openvpn --config client.ovpn --daemon .
 dotnet /app/webapp.dll
