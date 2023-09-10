850e2a848839922a365bc70810e33acf23028906
Muhammad Ahsan Khan <56512649+mahsankhaan@users.noreply.github.com>
2023-09-10 19:49:24 +0500

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -375,6 +375,6 @@ else
     [[ -e $cert ]] || grep -Eq '^ *(<ca>|ca +)' $conf ||
         { echo "ERROR: VPN CA cert missing!"; sleep 120; }
     set -x
-    exec sg vpn -c "openvpn --cd $dir --config $conf $ext_args \
+    sudo exec sg vpn -c "openvpn --cd $dir --config $conf $ext_args \
                ${OTHER_ARGS:-} ${MSS:+--fragment $MSS --mssfix}"
 fi
